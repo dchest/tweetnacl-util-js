6a04f9a591d23cd5ab1c8a65209651b9adfe072c
Dmitry Chestnykh <dmitry@codingrobots.com>
2017-03-13 23:13:06 +0100

    Fix style

--- nacl-util.js
+++ nacl-util.js
@@ -31,7 +31,7 @@
 
     var validateBase64 = function(s) {
       if (!(/^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/.test(s))) {
-        throw new TypeError("invalid encoding");
+        throw new TypeError('invalid encoding');
       }
     }
 
