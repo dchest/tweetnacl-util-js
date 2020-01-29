7661fc5445eaf299086effb09cbab4486832c630
Gervasio Junior <jamilservicos@gmail.com>
2020-01-29 20:30:34 -0300

    Update nacl-util.js
    fix maximum call stack exceeded

--- nacl-util.js
+++ nacl-util.js
@@ -14,7 +14,7 @@
   var util = {};
 
   function validateBase64(s) {
-    if (!(/^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/.test(s))) {
+    if (!(/^(?:[A-Za-z0-9+\/]{2}[A-Za-z0-9+\/]{2})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$/.test(s))) {
       throw new TypeError('invalid encoding');
     }
   }
